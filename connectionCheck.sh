c500b933c4bcf810bde0c826f2a67b8dd5afca49
Juha Kervinen <joker@iki.fi>
2022-03-20 17:50:16 +0200

    Append to log file instead of overwrite

--- connectionCheck.sh
+++ connectionCheck.sh
@@ -6,11 +6,11 @@ ping -c4 192.168.1.1 > /dev/null
 
 if [ $? != 0 ]
 then
-  echo "$timestamp - Can't react $GATEWAY, restarting wlan0..." > $logfile
+  echo "$timestamp - Can't react $GATEWAY, restarting wlan0..." >> $logfile
   /sbin/ifdown 'wlan0'
   sleep 5
   /sbin/ifup --force 'wlan0'
 else
-  echo "$timestamp - Connection ok" > $logfile
+  echo "$timestamp - Connection ok" >> $logfile
 fi
 
