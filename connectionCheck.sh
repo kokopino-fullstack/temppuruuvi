a79bb5763ba3a5f257eaad1728968728d4502cfd
Juha Kervinen <joker@iki.fi>
2022-03-20 16:57:34 +0200

    Add script to perform connectivity check

---
+++ connectionCheck.sh
@@ -0,0 +1,16 @@
+GATEWAY=192.168.1.1
+timestamp=$(date)
+logfile=~/connection.log
+touch $logfile
+ping -c4 192.168.1.1 > /dev/null
+
+if [ $? != 0 ]
+then
+  echo "$timestamp - Can't react $GATEWAY, restarting wlan0..." > $logfile
+  /sbin/ifdown 'wlan0'
+  sleep 5
+  /sbin/ifup --force 'wlan0'
+else
+  echo "$timestamp - Connection ok" > $logfile
+fi
+
